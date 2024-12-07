9a757f85f702821a264d911d1f21404e21d5ae9b
Ajmal Ali <ajmalali@Itadmins-MacBook-Pro.local>
2024-12-08 00:44:33 +0530

    fix responsive issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import FormBuilder from "@/components/form-builder";
 
 export default function Home() {
   return (
-    <div className="max-w-4xl mx-auto flex items-center justify-center px-20">
+    <div className="max-w-4xl mx-auto flex items-center justify-center md:px-20">
       <main className="w-full bg-white border-x border-[#E1E4E8] h-screen relative">
         <FormBuilder />
       </main>
