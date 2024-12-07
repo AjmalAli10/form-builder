cbf2ddbd3496962009e9542d7af0451eaf1b7a29
Ajmal Ali <ajmalali@Itadmins-MacBook-Pro.local>
2024-12-07 21:32:07 +0530

    added width and height

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
 
 export default function RootLayout({
   children,
@@ -9,6 +10,15 @@ export default function RootLayout({
     <html lang="en">
       <body className="bg-white text-gray-600">
         {children}
+        <Toaster
+          position="bottom-center"
+          toastOptions={{
+            style: {
+              maxWidth: '500px',
+              margin: '0 auto 20px auto',
+            },
+          }}
+        />
       </body>
     </html>
   );
